d3c2bae707a4c3e3d685d0561a08a8a6b884dd87
codeyjoseph <kearneyjoe2@outlook.com>
2024-11-04 14:24:42 +0000

    update2

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
 	System.out.println(sayHelloToSomeone("Joe"));
+	System.out.println(sayHelloToSomeone("PLEASE WORK"));
         System.out.println(sayGoodbye());
     }
 
